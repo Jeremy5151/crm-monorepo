eb9be099e6ac95c5b9752bd93ce58b496a7acfbe
Jeremy Woremy <jeremyworemy@gmail.com>
2025-10-27 15:48:55 +0100

    Fix: Export AuthGuard from AuthModule to make it available in GroupsModule

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { AuthGuard } from './auth.guard';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
-  providers: [AuthService],
+  providers: [AuthService, AuthGuard],
   controllers: [AuthController],
-  exports: [AuthService],
+  exports: [AuthService, AuthGuard],
 })
 export class AuthModule {}
--- apps/api/src/groups/groups.module.ts
+++ apps/api/src/groups/groups.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { GroupsController } from './groups.controller';
 import { GroupsService } from './groups.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   controllers: [GroupsController],
   providers: [GroupsService],
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   exports: [GroupsService]
 })
 export class GroupsModule {}
